2387e3f40a51efc7db39f2fd0b8ab111af9c001c
Dylan Waits <dylan@waits.io>
2016-03-14 18:54:53 -0700

    Load list of sites to test from a JSON config file

--- status.py
+++ status.py
@@ -1,10 +1,23 @@
 from flask import Flask
 from flask import abort, redirect, render_template, url_for
+import json
+import requests
+
 app = Flask(__name__)
+with open('config.json') as file:
+    config = json.load(file)
+    file.close()
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    sites = config['sites']
+    for site in sites:
+        try:
+            r = requests.get(site['url'])
+            site['status'] = r.status_code
+        except requests.exceptions.ConnectionError:
+            site['status'] = 0
+    return render_template('index.html', sites=sites)
 
 if __name__ == '__main__':
     app.debug = True
